0ae15da17a88c63dfe82c9f1b780110d8d00855b
MudayJ <02jensen.muday@gulllakecs.org>
2024-01-19 07:38:51 -0500

    square w/ different colors for each line

--- Sandbox.js
+++ Sandbox.js
@@ -1,13 +1,57 @@
 //Programmer: Jensen Muday
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
-//Bolt ID: SF28B
+//Bolt ID: SBF28B
 
+/*
 async function startProgram() {
 	await speak("Hello World", true);
-	setMainLed({ r: 0, g: 0, b: 255});
+	setMainLed({ r: 145, g: 0, b: 100});
 	setSpeed(60);
 	await delay (2);
 	setSpeed(0);
-
 }
+*/
+
+// Square w/ different colors for each line & commit
+//Date: 1.19.2024
+async function startProgram() {
+	for (var _i1 = 0; _i1 < 8; _i1++) {
+		setMainLed(209,19,19)
+		await roll(90, 50, 2)
+		setMainLed(255,205,3)
+		await roll(180, 50, 2)	
+		setMainLed(78,255,3)
+		await roll(270, 50, 2)	
+		setMainLed(54,190,227)
+		await roll(360, 50, 2)	
+		
+	
+
+	}
+}
+
+// Square w/ different colors and sounds for each line & commit
+/*
+
+
+
+
+//Figure 8
+//1.19.2024
+async function startProgram() {
+	for (var _i1 = 0; _i1 < 8; _i1++) {
+		setMainLed(209,19,19)
+		await roll(90, 50, 2)
+		setMainLed(255,205,3)
+		await roll(180, 50, 2)	
+		setMainLed(78,255,3)
+		await roll(270, 50, 2)	
+		setMainLed(54,190,227)
+		await roll(360, 50, 2)	
+		
+	
+
+	}
+}
+*/
