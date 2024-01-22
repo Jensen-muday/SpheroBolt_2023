2a85288e2c6994d5c090d4b03a16409e1634fa07
MudayJ <02jensen.muday@gulllakecs.org>
2024-01-22 08:35:42 -0500

    1.22.2024

---
+++ Maze.js
@@ -0,0 +1,10 @@
+// Team members: Jensen Muday, Brian Kuhn
+// Name Jensen Muday
+// Date: 1.22.2024
+// Program: Maze
+// Bolt ID: SBF28B
+
+async function startProgram() {
+	await scrollMatrixText('Start!', {r:9, g:255, b:123}, 25, true)
+	await speak('Start')
+}
