070cc3a81c22967a5ad2f5816cbce713d7222c70
MudayJ <02jensen.muday@gulllakecs.org>
2024-01-24 08:05:14 -0500

    1.24.2024
    rolls to blue LED area.

--- Maze.js
+++ Maze.js
@@ -2,10 +2,16 @@
 // Name Jensen Muday
 // Date: 1.22.2024
 // Program: Maze
-// Bolt ID: SBF28B
-
+// Bolt ID: SB-F28B
+/*
 //initial start message
 async function startProgram() {
 	await scrollMatrixText('Start!', {r:9, g:255, b:123}, 25, true)
 	await speak('Start')
+}
+*/
+//Roll to Blue then Blue LED
+async function startProgram() {
+	await roll(0, 105, 1.5)
+	setMainLed({r:1, g:1, b:253})
 }
